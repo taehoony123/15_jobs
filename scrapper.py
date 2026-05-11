b40bc67a004a1a47a1aa4b3b42180fb073302d9e
taehoony123 <taehun647@gmail.com>
2026-05-11 17:45:29 +0900

    0511 1744

--- scrapper.py
+++ scrapper.py
@@ -8,4 +8,22 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-soup.find_all()
+lis = soup.find_all("li", class_="c_col")
+# print(len(lis))
+# print(lis)
+
+for li in lis[0:3]:
+    # company = li.find("a", class_="cpname").text.strip()
+    # print(company)
+
+    # title = li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").text.strip()
+    # print(title)
+
+    # location = li.find("div", class_="cell_mid").find("div", class_="cl_md").find_all("span")[0].text.strip()
+    # print(location)
+
+    # career = li.find("div", class_="cell_mid").find("div", class_="cl_md").find_all("span")[1].text.strip()
+    # print(career)
+
+    link = li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").get("href")
+    print(link)
